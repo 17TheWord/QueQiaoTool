cc438e9b39065a432d79cb0436d6047107034ca6
17TheWord <17theword@gmail.com>
2024-08-05 17:14:28 +0800

    :white_check_mark: Test config and websocket server and encode server name

---
+++ src/test/java/queqiao/utils/ConfigTest.java
@@ -0,0 +1,40 @@
+package queqiao.utils;
+
+import com.github.theword.queqiao.utils.Config;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+
+class ConfigTest {
+    private static final Logger logger = LoggerFactory.getLogger(ConfigTest.class);
+
+    @Test
+    void testLoadConfig() {
+        Config config;
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.yml")) {
+            Yaml yaml = new Yaml();
+            config = yaml.loadAs(inputStream, Config.class);
+            System.out.println(config.getServer_name());
+        } catch (Exception e) {
+            logger.warn(e.getMessage());
+        }
+    }
+
+    @Test
+    void testLoadConfigWithException() {
+        Config config;
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.yml")) {
+            Yaml yaml = new Yaml();
+            config = yaml.loadAs(inputStream, Config.class);
+            System.out.println(config.getServer_name());
+            throw new RuntimeException("Test config with RuntimeException.");
+        } catch (Exception e) {
+            config = new Config();
+            System.out.println(config.getServer_name());
+            logger.warn(e.getMessage());
+        }
+    }
+}
---
+++ src/test/java/queqiao/websocket/WsServerTest.java
@@ -0,0 +1,41 @@
+package queqiao.websocket;
+
+import com.github.theword.queqiao.websocket.WsServer;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import static com.github.theword.queqiao.utils.Tool.logger;
+
+class WsServerTest {
+
+    @Test
+    void testUnicodeServerName() throws UnsupportedEncodingException {
+        System.out.println(URLEncoder.encode("服务器", StandardCharsets.UTF_8.toString()));
+    }
+
+    @Test
+    void testDecodeServerName() throws UnsupportedEncodingException {
+        System.out.println(URLDecoder.decode("%E6%9C%8D%E5%8A%A1%E5%99%A8", StandardCharsets.UTF_8.toString()));
+    }
+
+    @Test
+    void testOnOpen() {
+        logger = LoggerFactory.getLogger(WsServerTest.class);
+        InetSocketAddress inetSocketAddress = new InetSocketAddress("127.0.0.1", 25565);
+        WsServer wsServer = new WsServer(inetSocketAddress);
+        wsServer.start();
+        try {
+            Thread.sleep(3000);
+            wsServer.stop();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
