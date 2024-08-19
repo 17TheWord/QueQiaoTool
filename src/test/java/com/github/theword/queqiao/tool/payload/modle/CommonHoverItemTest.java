4df55d08bf02c78143705258de3d1fec093acba7
17TheWord <17theword@gmail.com>
2024-08-19 19:18:43 +0800

    test(payload): 新增CommonHoverItem的单元测试
    新增一个单元测试类CommonHoverItemTest，使用Gson对CommonHoverItem进行反序列化测试。测试确保
    CommonHoverItem对象的id、count、tag和key属性能够正确解析并打印到控制台。

---
+++ src/test/java/com/github/theword/queqiao/tool/payload/modle/CommonHoverItemTest.java
@@ -0,0 +1,19 @@
+package com.github.theword.queqiao.tool.payload.modle;
+
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Test;
+
+
+class CommonHoverItemTest {
+
+    @Test
+    void testHoverItem() {
+        Gson gson = new Gson();
+        CommonHoverItem commonHoverItem = gson.fromJson("{\"id\": 1,\"count\": 1,\"tag\": \"tag\", \"key\": \"minecraft:dirt\"}", CommonHoverItem.class);
+        System.out.println(commonHoverItem.getId());
+        System.out.println(commonHoverItem.getCount());
+        System.out.println(commonHoverItem.getTag());
+        System.out.println(commonHoverItem.getKey());
+    }
+
+}
