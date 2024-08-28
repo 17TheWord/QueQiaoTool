5b989f70bd980308b6be31b105429c3968fbeada
17TheWord <17theword@gmail.com>
2024-08-28 20:54:34 +0800

    refactor(payloads): 调整基类并改进私信载荷表示逻辑
    - 增加BasePayload类中的echo字段，用于处理请求结果和返回值。- 文档化BasePayload类以澄清其属性的用途。
    -重构PrivateMessagePayload类中的目标玩家识别逻辑，优先使用nickname和UUID。
    - 格式化PrivateMessagePayload类中的toString方法，以提升输出的可读性。
    
    BREAKING CHANGE: 调整了BasePayload和PrivateMessagePayload的结构，可能影响依赖于先前类结构的客户端代码。

--- src/main/java/com/github/theword/queqiao/tool/payload/BasePayload.java
+++ src/main/java/com/github/theword/queqiao/tool/payload/BasePayload.java
@@ -3,11 +3,15 @@ package com.github.theword.queqiao.tool.payload;
 import com.google.gson.JsonElement;
 import lombok.Data;
 
+/**
+ * BasePayload
+ * <p>api：API名称</p>
+ * <p>data：Json对象，在 {@link com.github.theword.queqiao.tool.handle.HandleProtocolMessage } 中处理</p>
+ * <p>echo：用于请求结果以及返回值</p>
+ */
 @Data
 public class BasePayload {
-
     private String api;
-
     private JsonElement data;
-
+    private String echo;
 }
