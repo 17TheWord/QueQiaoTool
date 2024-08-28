c4bad08f8bd204e32cb210d7238f50b52c3b8727
17TheWord <17theword@gmail.com>
2024-08-28 20:54:23 +0800

    refactor(websocket): 重构WebSocket私聊消息处理
    重构WebSocket消息处理逻辑，将私聊消息处理从`HandleProtocolMessage`移动到`HandleApiService`。此外，`ParseJsonToEventService`现在定义了用于解析消息的新方法，以适应不同的文本组件需求。
    
    BREAKING CHANGE: 私聊消息处理的实现需要更新以适应`HandleApiService`中的新方法签名。消息解析的逻辑现在通过`ParseJsonToEventService`暴露，可能需要对客户端消息处理进行适应性更改。

--- src/main/java/com/github/theword/queqiao/tool/handle/HandleApiService.java
+++ src/main/java/com/github/theword/queqiao/tool/handle/HandleApiService.java
@@ -2,46 +2,40 @@ package com.github.theword.queqiao.tool.handle;
 
 import com.github.theword.queqiao.tool.payload.MessageSegment;
 import com.github.theword.queqiao.tool.payload.TitlePayload;
-import org.java_websocket.WebSocket;
+import com.github.theword.queqiao.tool.response.PrivateMessageResponse;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface HandleApiService {
 
-    ParseJsonToEventService parseJsonToEventService = null;
-
     /**
      * 广播消息
      *
-     * @param webSocket   WebSocket
      * @param messageList 消息列表
      */
-    void handleBroadcastMessage(WebSocket webSocket, List<MessageSegment> messageList);
+    void handleBroadcastMessage(List<MessageSegment> messageList);
 
     /**
-     * 广播 Send Title 消息
+     * 广播 Title 消息
      *
-     * @param webSocket    WebSocket
      * @param titlePayload Title
      */
-    void handleSendTitleMessage(WebSocket webSocket, TitlePayload titlePayload);
+    void handleSendTitleMessage(TitlePayload titlePayload);
 
     /**
-     * 广播 Action Bar 消息
+     * 广播 ActionBar 消息
      *
-     * @param webSocket   WebSocket
      * @param messageList Action Bar 消息列表
      */
-    void handleActionBarMessage(WebSocket webSocket, List<MessageSegment> messageList);
+    void handleSendActionBarMessage(List<MessageSegment> messageList);
 
     /**
      * 发送私聊消息
      *
-     * @param webSocket        WebSocket
-     * @param targetPlayerName 目标玩家名
-     * @param targetPlayerUuid 目标 UUID
-     * @param messageList      消息列表
+     * @param nickname    目标玩家名
+     * @param uuid        目标 UUID
+     * @param messageList 消息列表
      */
-    void handlePrivateMessage(WebSocket webSocket, String targetPlayerName, UUID targetPlayerUuid, List<MessageSegment> messageList);
+    PrivateMessageResponse handleSendPrivateMessage(String nickname, UUID uuid, List<MessageSegment> messageList);
 }
--- src/main/java/com/github/theword/queqiao/tool/handle/ParseJsonToEventService.java
+++ src/main/java/com/github/theword/queqiao/tool/handle/ParseJsonToEventService.java
@@ -5,10 +5,26 @@ import com.github.theword.queqiao.tool.payload.modle.component.CommonTextCompone
 
 import java.util.List;
 
+/**
+ * 该接口只统一方法名
+ * <p>具体返回值类型由服务端自行提供</p>
+ */
 public interface ParseJsonToEventService {
 
+    /**
+     * 解析消息列表
+     *
+     * @param messageList MessageSegment 消息列表
+     * @return 服务端具体文本组件
+     */
     Object parseMessageListToComponent(List<MessageSegment> messageList);
 
+    /**
+     * 解析消息
+     *
+     * @param message 统一自定义文本组件
+     * @return 服务端具体文本组件
+     */
     Object parsePerMessageToComponent(CommonTextComponent message);
 
 }
