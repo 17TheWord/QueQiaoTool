682272683fcee1ca00a03667c07fd61eb9144a27
17TheWord <17theword@gmail.com>
2024-08-28 20:54:03 +0800

    ```fix(queqiao): 确保UUID和昵称正确反序列化到PrivateMessagePayload
    在MessagePayload的反序列化过程中，由于未能正确识别并处理含有UUID和昵称的PrivateMessagePayload，已应用一个修复。反序列化逻辑现能正确识别并处理这些字段，将它们分配给PrivateMessagePayload类的实例。
    ```

--- src/main/java/com/github/theword/queqiao/tool/deserializer/MessagePayloadDeserializer.java
+++ src/main/java/com/github/theword/queqiao/tool/deserializer/MessagePayloadDeserializer.java
@@ -1,20 +1,40 @@
 package com.github.theword.queqiao.tool.deserializer;
 
 import com.github.theword.queqiao.tool.payload.MessagePayload;
+import com.github.theword.queqiao.tool.payload.PrivateMessagePayload;
 import com.github.theword.queqiao.tool.utils.PayloadUtils;
 import com.google.gson.*;
 
 import java.lang.reflect.Type;
+import java.util.UUID;
 
 public class MessagePayloadDeserializer implements JsonDeserializer<MessagePayload> {
 
+    /**
+     * 反序列化Message消息
+     * <p>接管所有 MessagePayload 及其子类的反序列化任务</p>
+     *
+     * @param json    Json数据
+     * @param typeOfT 目标类型
+     * @param context Json反序列化上下文
+     * @return ? extends MessagePayload
+     * @throws JsonParseException Json反序列化异常
+     */
     @Override
     public MessagePayload deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        MessagePayload payload = new MessagePayload();
+        JsonObject jsonObject = json.getAsJsonObject();
 
-        JsonElement messageElement = json.getAsJsonObject().get("message");
-        payload.setMessage(PayloadUtils.deserializeMessageSegmentList(messageElement, context));
-
-        return payload;
+        if (jsonObject.has("uuid") || jsonObject.has("nickname")) {
+            PrivateMessagePayload privatePayload = new PrivateMessagePayload();
+            privatePayload.setUuid(context.deserialize(jsonObject.get("uuid"), UUID.class));
+            privatePayload.setNickname(context.deserialize(jsonObject.get("nickname"), String.class));
+            privatePayload.setMessage(PayloadUtils.deserializeMessageSegmentList(jsonObject.get("message"), context));
+            return privatePayload;
+        } else {
+            MessagePayload payload = new MessagePayload();
+            JsonElement messageElement = jsonObject.get("message");
+            payload.setMessage(PayloadUtils.deserializeMessageSegmentList(messageElement, context));
+            return payload;
+        }
     }
 }
