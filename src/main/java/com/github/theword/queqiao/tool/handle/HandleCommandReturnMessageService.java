5eb8658fbf205b0aa1bc272af38a9c1b105e9eb3
17TheWord <17theword@gmail.com>
2024-09-10 19:39:25 +0800

    在`HandleCommandReturnMessageService`接口中添加了`hasPermission`方法，用于检查命令返回者是否具有指定的权限节点权限。

--- src/main/java/com/github/theword/queqiao/tool/handle/HandleCommandReturnMessageService.java
+++ src/main/java/com/github/theword/queqiao/tool/handle/HandleCommandReturnMessageService.java
@@ -9,4 +9,13 @@ public interface HandleCommandReturnMessageService {
      * @param message         返回消息
      */
     void handleCommandReturnMessage(Object commandReturner, String message);
+
+    /**
+     * 判断是否拥有权限
+     *
+     * @param commandReturner 命令返回者
+     * @param permissionNode  权限节点
+     * @return 是否拥有权限
+     */
+    boolean hasPermission(Object commandReturner, String permissionNode);
 }
