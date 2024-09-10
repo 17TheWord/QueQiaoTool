fdf409b42adef39b2b1b99c2731ba3443c7f5424
17TheWord <17theword@gmail.com>
2024-09-10 19:39:41 +0800

    新命令抽象类ClientCommandAbstract创建
    在queqiao项目的工具命令子系统中，新增了一个抽象类ClientCommandAbstract，用于定义Websocket客户端相关命令的基础。该类实现了SubCommand接口，提供了命令的基本信息，如名称、前缀、描述和用法，并定义了执行命令的默认行为。此更改将作为创建具体Websocket客户端命令的基础模板。

---
+++ src/main/java/com/github/theword/queqiao/tool/command/subCommand/ClientCommandAbstract.java
@@ -0,0 +1,82 @@
+package com.github.theword.queqiao.tool.command.subCommand;
+
+import com.github.theword.queqiao.tool.command.SubCommand;
+import com.github.theword.queqiao.tool.constant.BaseConstant;
+
+public class ClientCommandAbstract implements SubCommand {
+
+    /**
+     * 获取命令名称
+     *
+     * @return client
+     */
+    @Override
+    public String getName() {
+        return "client";
+    }
+
+    /**
+     * 获取命令前缀
+     * <p>用于遍历时判断前驱后继</p>
+     * <P>为空字符串则代表根命令</P>
+     *
+     * @return {@link BaseConstant#COMMAND_HEADER}
+     */
+    @Override
+    public String getPrefix() {
+        return BaseConstant.COMMAND_HEADER;
+    }
+
+    /**
+     * 获取命令描述
+     *
+     * @return Websocket Client 命令
+     */
+    @Override
+    public String getDescription() {
+        return "Websocket Client 命令";
+    }
+
+    /**
+     * 获取命令用法
+     *
+     * @return 使用：/{@link BaseConstant#COMMAND_HEADER} client
+     */
+    @Override
+    public String getUsage() {
+        return "使用：/" + BaseConstant.COMMAND_HEADER + " client";
+    }
+
+    /**
+     * 获取命令权限节点
+     *
+     * @return {@link BaseConstant#COMMAND_HEADER}.client
+     */
+    @Override
+    public String getPermissionNode() {
+        return BaseConstant.COMMAND_HEADER + ".client";
+    }
+
+    /**
+     * 执行命令
+     * <p>Pass</p>
+     *
+     * @param commandReturner 命令执行者
+     * @param boolVar         布尔值占位符
+     */
+    @Override
+    public void execute(Object commandReturner, boolean boolVar) {
+        // pass
+    }
+
+    /**
+     * 执行命令
+     * <p>Pass</p>
+     *
+     * @param commandReturner 命令执行者
+     */
+    @Override
+    public void execute(Object commandReturner) {
+        // pass
+    }
+}
