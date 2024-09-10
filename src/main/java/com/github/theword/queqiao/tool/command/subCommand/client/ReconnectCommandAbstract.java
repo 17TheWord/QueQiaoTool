fb13406551f75d1ea9098c948e0d5aa11c938d32
17TheWord <17theword@gmail.com>
2024-09-10 19:33:28 +0800

    抽象类`ReconnectCommandAbstract`已进行重构，以增强其功能和可读性。本次更改包括：
    - 继承自`ClientCommandAbstract`，统一客户端命令的结构。
    - 移除多余的导入语句，简化代码依赖。
    - 重写`execute`方法，以支持可选的“全部重连”参数。
    - 内部逻辑调整，使用`AtomicInteger`来追踪已打开的WebSocket客户端，避免不必要的重连操作。
    -集成`Tool`类的静态方法，优化命令返回信息的处理。
    
    这些更改使代码更加清晰，并提升了执行重连命令时的效率。

--- src/main/java/com/github/theword/queqiao/tool/command/subCommand/client/ReconnectCommandAbstract.java
+++ src/main/java/com/github/theword/queqiao/tool/command/subCommand/client/ReconnectCommandAbstract.java
@@ -1,9 +1,16 @@
 package com.github.theword.queqiao.tool.command.subCommand.client;
 
-import com.github.theword.queqiao.tool.command.SubCommand;
-import com.github.theword.queqiao.tool.constant.BaseConstant;
+import com.github.theword.queqiao.tool.command.subCommand.ClientCommandAbstract;
+import com.github.theword.queqiao.tool.constant.CommandConstantMessage;
+import com.github.theword.queqiao.tool.utils.Tool;
+import com.github.theword.queqiao.tool.websocket.WsClient;
 
-public abstract class ReconnectCommandAbstract implements SubCommand {
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.github.theword.queqiao.tool.utils.Tool.websocketManager;
+
+public abstract class ReconnectCommandAbstract extends ClientCommandAbstract {
 
     /**
      * 获取命令名称
@@ -18,30 +25,71 @@ public abstract class ReconnectCommandAbstract implements SubCommand {
     /**
      * 获取命令描述
      *
-     * @return 重新连接 Websocket Clients。
+     * @return 重新连接 Websocket Clients
      */
     @Override
     public String getDescription() {
-        return "重新连接 Websocket Clients。";
+        return "重新连接 Websocket Clients";
     }
 
     /**
      * 获取命令用法
      *
-     * @return 使用：/{@link BaseConstant#COMMAND_HEADER} client reconnect [all]
+     * @return 使用：/{@link ClientCommandAbstract#getUsage()} reconnect [all]
      */
     @Override
     public String getUsage() {
-        return "使用：/" + BaseConstant.COMMAND_HEADER + " client reconnect [all]";
+        return super.getUsage() + " reconnect [all]";
     }
 
     /**
      * 获取命令权限节点
      *
-     * @return {@link BaseConstant#COMMAND_HEADER}.client.reconnect
+     * @return {@link ClientCommandAbstract#getPermissionNode()}.reconnect
      */
     @Override
     public String getPermissionNode() {
-        return BaseConstant.COMMAND_HEADER + ".client.reconnect";
+        return super.getPermissionNode() + ".reconnect";
+    }
+
+    /**
+     * 执行命令
+     * <p>Pass</p>
+     *
+     * @param commandReturner 命令执行者
+     */
+    @Override
+    public void execute(Object commandReturner) {
+        execute(commandReturner, false);
+    }
+
+    /**
+     * 重连 WebSocket 客户端
+     * reconnect [all] 命令调用
+     *
+     * @param all             是否全部重连
+     * @param commandReturner 命令执行者
+     */
+    @Override
+    public void execute(Object commandReturner, boolean all) {
+        String reconnectCount = all ? CommandConstantMessage.RECONNECT_ALL_CLIENT : CommandConstantMessage.RECONNECT_NOT_OPEN_CLIENT;
+        Tool.commandReturn(commandReturner, reconnectCount);
+
+        AtomicInteger opened = new AtomicInteger();
+
+        List<WsClient> wsClientList = websocketManager.getWsClientList();
+
+        wsClientList.forEach(wsClient -> {
+            if (all || !wsClient.isOpen()) {
+                wsClient.reconnectWebsocket();
+                Tool.commandReturn(commandReturner, String.format(CommandConstantMessage.RECONNECT_MESSAGE, wsClient.getURI()));
+            } else {
+                opened.getAndIncrement();
+            }
+        });
+        if (opened.get() == wsClientList.size()) {
+            Tool.commandReturn(commandReturner, CommandConstantMessage.RECONNECT_NO_CLIENT_NEED_RECONNECT);
+        }
+        Tool.commandReturn(commandReturner, CommandConstantMessage.RECONNECTED);
     }
 }
