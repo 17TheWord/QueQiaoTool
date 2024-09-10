92d8bc1ebfc417d0afe4ad7cc7d6e3e0c8439cb6
17TheWord <17theword@gmail.com>
2024-09-10 19:42:59 +0800

    refactor(websocket): 将WebSocket管理器方法设置为public并重构客户端重启逻辑
    - 将`restartWebsocketClients`和`restartWebsocketServer`方法的访问级别从private更改为public。
    -重构了WebSocket客户端和服务器重启逻辑，优化了代码结构。
    -移除了未使用的导入和变量，如`AtomicInteger`和迭代器。
    - 简化了重新连接和状态检查逻辑，删除了冗余代码。
    - 通过移除不必要的参数和逻辑，精简了`reloadWebsocket`和`reconnectWebsocketClients`方法。- 更新了命令返回消息，使其更具描述性。
    
    这些更改使WebSocket管理器的API更加一致和易于使用，同时减少了代码复杂性，提高了可维护性。

--- src/main/java/com/github/theword/queqiao/tool/utils/WebsocketManager.java
+++ src/main/java/com/github/theword/queqiao/tool/utils/WebsocketManager.java
@@ -1,19 +1,15 @@
 package com.github.theword.queqiao.tool.utils;
 
-import com.github.theword.queqiao.tool.constant.CommandConstantMessage;
 import com.github.theword.queqiao.tool.constant.WebsocketConstantMessage;
 import com.github.theword.queqiao.tool.websocket.WsClient;
 import com.github.theword.queqiao.tool.websocket.WsServer;
 import lombok.Getter;
-import org.java_websocket.WebSocket;
 
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.github.theword.queqiao.tool.utils.Tool.config;
 
@@ -72,7 +68,7 @@ public class WebsocketManager {
      *
      * @param commandReturner 命令执行者
      */
-    private void restartWebsocketClients(Object commandReturner) {
+    public void restartWebsocketClients(Object commandReturner) {
         Tool.commandReturn(commandReturner, WebsocketConstantMessage.Client.RELOADING);
         stopWebsocketClients(1000, WebsocketConstantMessage.CLOSE_BY_RELOAD, commandReturner);
         startWebsocketClients(commandReturner);
@@ -117,7 +113,7 @@ public class WebsocketManager {
      *
      * @param commandReturner 命令执行者
      */
-    private void restartWebsocketServer(Object commandReturner) {
+    public void restartWebsocketServer(Object commandReturner) {
         stopWebsocketServer(commandReturner, WebsocketConstantMessage.Server.RELOADING);
         startWebsocketServer(commandReturner);
         Tool.commandReturn(commandReturner, WebsocketConstantMessage.Server.RELOADED);
@@ -153,105 +149,5 @@ public class WebsocketManager {
     public void stopWebsocket(int code, String reason, Object commandReturner) {
         stopWebsocketClients(code, reason, commandReturner);
         stopWebsocketServer(commandReturner, reason);
-    }
-
-
-    /**
-     * 重载 WebSocket
-     * reload 命令调用
-     *
-     * @param isModServer     是否为 ModServer
-     * @param commandReturner 命令执行者
-     */
-    public void reloadWebsocket(boolean isModServer, Object commandReturner) {
-        config = Config.loadConfig(isModServer);
-        Tool.commandReturn(commandReturner, CommandConstantMessage.RELOAD_CONFIG);
-        restartWebsocketServer(commandReturner);
-        restartWebsocketClients(commandReturner);
-    }
-
-    /**
-     * 重连 WebSocket 客户端
-     * reconnect [all] 命令调用
-     *
-     * @param all             是否全部重连
-     * @param commandReturner 命令执行者
-     */
-    public void reconnectWebsocketClients(boolean all, Object commandReturner) {
-        String reconnectCount = all ? CommandConstantMessage.RECONNECT_ALL_CLIENT : CommandConstantMessage.RECONNECT_NOT_OPEN_CLIENT;
-        Tool.commandReturn(commandReturner, reconnectCount);
-
-        AtomicInteger opened = new AtomicInteger();
-        wsClientList.forEach(wsClient -> {
-            if (all || !wsClient.isOpen()) {
-                wsClient.reconnectWebsocket();
-                Tool.commandReturn(commandReturner, String.format(CommandConstantMessage.RECONNECT_MESSAGE, wsClient.getURI()));
-            } else {
-                opened.getAndIncrement();
-            }
-        });
-        if (opened.get() == wsClientList.size()) {
-            Tool.commandReturn(commandReturner, CommandConstantMessage.RECONNECT_NO_CLIENT_NEED_RECONNECT);
-        }
-        Tool.commandReturn(commandReturner, CommandConstantMessage.RECONNECTED);
-    }
-
-
-    /**
-     * 获取 WebSocket 服务端状态
-     * 整合游戏内命令调用
-     *
-     * @param commandReturner 命令执行者
-     */
-    public void getWebsocketServerStatus(Object commandReturner) {
-        if (!config.getWebsocketServer().isEnable()) {
-            Tool.commandReturn(commandReturner, "Websocket Server 配置项未启用，如需开启，请在 config.yml 中启用 WebsocketServer 配置项");
-            Tool.commandReturn(commandReturner, String.format("配置项中地址为 %s:%d", config.getWebsocketServer().getHost(), config.getWebsocketServer().getPort()));
-            return;
-        }
-
-        if (wsServer == null) {
-            Tool.commandReturn(commandReturner, "Websocket Server 为null，查询失败");
-            return;
-        }
-
-        Tool.commandReturn(commandReturner, String.format("当前 Websocket Server 已开启，监听地址为 %s:%d", wsServer.getAddress().getHostString(), wsServer.getPort()));
-
-        if (wsServer.getConnections().isEmpty()) {
-            Tool.commandReturn(commandReturner, "当前暂无 Websocket 连接到该 Server");
-            return;
-        }
-
-        Tool.commandReturn(commandReturner, String.format("当前 Websocket Server 已有 %d 个连接", wsServer.getConnections().size()));
-
-        int count = 0;
-        for (WebSocket webSocket : wsServer.getConnections()) {
-            count++;
-            Tool.commandReturn(commandReturner, String.format("%d 来自 %s:%d 的连接", count, webSocket.getRemoteSocketAddress().getHostString(), webSocket.getRemoteSocketAddress().getPort()));
-        }
-    }
-
-    /**
-     * 获取 WebSocket 客户端状态
-     * 整合游戏内命令调用
-     *
-     * @param commandReturner 命令执行者
-     */
-    public void getWebsocketClientStatus(Object commandReturner) {
-        if (!config.getWebsocketClient().isEnable()) {
-            Tool.commandReturn(commandReturner, "Websocket Client 配置项未启用，如需开启，请在 config.yml 中启用 WebsocketClient 配置项");
-            Tool.commandReturn(commandReturner, "配置文件中连接列表如下共 " + config.getWebsocketClient().getUrlList().size() + " 个 Client");
-            for (int i = 0; i < config.getWebsocketClient().getUrlList().size(); i++) {
-                Tool.commandReturn(commandReturner, String.format("%d 连接至 %s", i + 1, config.getWebsocketClient().getUrlList().get(i)));
-            }
-            return;
-        }
-
-        Tool.commandReturn(commandReturner, "Websocket Client 列表，共 " + wsClientList.size() + " 个 Client");
-
-        for (int i = 0; i < wsClientList.size(); i++) {
-            WsClient wsClient = wsClientList.get(i);
-            Tool.commandReturn(commandReturner, String.format("%d 连接至 %s 的 Client，状态：%s", i, wsClient.getURI(), wsClient.isOpen() ? "已连接" : "未连接"));
-        }
     }
 }
