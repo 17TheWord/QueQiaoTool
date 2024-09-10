523a52cde5885ba298ccfb6886bf567c42cf0166
17TheWord <17theword@gmail.com>
2024-09-10 19:40:47 +0800

    新命令抽象类ServerCommandAbstract创建
    为WebSocket服务器命令创建了一个新的抽象类`ServerCommandAbstract`，该类实现了`SubCommand`接口。此抽象类定义了所有服务器命令的通用属性和行为，包括命令名称、描述、用法和权限节点。此外，还提供了默认的执行方法实现。

---
+++ src/main/java/com/github/theword/queqiao/tool/command/subCommand/ServerCommandAbstract.java
@@ -0,0 +1,82 @@
+package com.github.theword.queqiao.tool.command.subCommand;
+
+import com.github.theword.queqiao.tool.command.SubCommand;
+import com.github.theword.queqiao.tool.constant.BaseConstant;
+
+public class ServerCommandAbstract implements SubCommand {
+
+    /**
+     * 获取命令名称
+     *
+     * @return server
+     */
+    @Override
+    public String getName() {
+        return "server";
+    }
+
+    /**
+     * 获取命令前缀
+     * <p>用于遍历时判断前驱后继</p>
+     * <P>为空字符串则代表根命令</P>
+     *
+     * @return {@link BaseConstant#COMMAND_HEADER}
+     */
+    @Override
+    public String getPrefix() {
+        return BaseConstant.COMMAND_HEADER;
+    }
+
+    /**
+     * 获取命令描述
+     *
+     * @return Websocket Server 命令
+     */
+    @Override
+    public String getDescription() {
+        return "Websocket Server 命令";
+    }
+
+    /**
+     * 获取命令用法
+     *
+     * @return 使用：/{@link BaseConstant#COMMAND_HEADER} server
+     */
+    @Override
+    public String getUsage() {
+        return "使用：/" + BaseConstant.COMMAND_HEADER + " server";
+    }
+
+    /**
+     * 获取命令权限节点
+     *
+     * @return {@link BaseConstant#COMMAND_HEADER}.server
+     */
+    @Override
+    public String getPermissionNode() {
+        return BaseConstant.COMMAND_HEADER + ".server";
+    }
+
+    /**
+     * 执行命令
+     * <p>位于本命令 pass</p>
+     *
+     * @param commandReturner 命令执行者
+     * @param boolVar         布尔值占位符
+     */
+    @Override
+    public void execute(Object commandReturner, boolean boolVar) {
+        // pass
+    }
+
+    /**
+     * 执行命令
+     * <p>位于本命令 pass</p>
+     *
+     * @param commandReturner 命令执行者
+     */
+    @Override
+    public void execute(Object commandReturner) {
+        // pass
+    }
+}
