23b41dc7b76992dd18d6f53de82a754f8a157516
17TheWord <17theword@gmail.com>
2025-09-25 12:43:12 +0800

    :white_check_mark: Disable client

--- src/test/java/com/github/theword/queqiao/tool/websocket/WsClientTest.java
+++ src/test/java/com/github/theword/queqiao/tool/websocket/WsClientTest.java
@@ -26,7 +26,7 @@ class WsClientTest {
         System.out.println("URLDecoder:" + URLDecoder.decode("%E6%9C%8D%E5%8A%A1%E5%99%A8", StandardCharsets.UTF_8.toString()));
     }
 
-    @Test
+//    @Test
     void testClient() throws URISyntaxException {
         URI uri = new URI("ws://localhost:8080/minecraft/ws");
         WsClient wsClient = new WsClient(
