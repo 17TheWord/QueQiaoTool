02a4ef38d83ff894ac151b842499f3d68f0ff12b
17TheWord <17theword@gmail.com>
2025-11-25 20:49:21 +0800

    :sparkles: 修复消息处理逻辑，简化响应返回，增加 API 字段支持

--- src/main/java/com/github/theword/queqiao/tool/handle/HandleProtocolMessage.java
+++ src/main/java/com/github/theword/queqiao/tool/handle/HandleProtocolMessage.java
@@ -66,7 +66,11 @@ public class HandleProtocolMessage {
     private Response handle(String rawJsonMessage, String address, MessageSource source) {
         debugLog("收到来自 {} 的 {} 消息：{}", address, source.toString(), rawJsonMessage);
         try {
-            return this.parseAndHandle(rawJsonMessage);
+            BasePayload basePayload = gson.fromJson(rawJsonMessage, BasePayload.class);
+            Response response = this.parseAndHandle(basePayload);
+            response.setApi(basePayload.getApi());
+            response.setEcho(basePayload.getEcho());
+            return response;
         } catch (Exception e) {
             this.logger.error("解析来自 {} 的 webSocket 消息时出现问题，消息内容： {}", address, rawJsonMessage);
             this.logger.error("错误信息：", e);
@@ -81,61 +85,58 @@ public class HandleProtocolMessage {
      *
      * @return Response 处理结果
      */
-    public Response parseAndHandle(String rawJsonMessage) {
-        BasePayload basePayload = gson.fromJson(rawJsonMessage, BasePayload.class);
-
+    public Response parseAndHandle(BasePayload basePayload) {
         String api = basePayload.getApi();
         JsonElement data = basePayload.getData();
-        String echo = basePayload.getEcho();
 
-        switch (api) {
+        switch (basePayload.getApi()) {
             case "broadcast":
             case "send_msg": {
                 MessagePayload messagePayload = gson.fromJson(data, MessagePayload.class);
                 handleApiService.handleBroadcastMessage(messagePayload.getMessage());
-                return Response.success(null, echo);
+                return Response.success();
             }
             case "send_title": {
                 TitlePayload titlePayload = gson.fromJson(data, TitlePayload.class);
                 if ((titlePayload.getTitle() == null || titlePayload.getTitle().isJsonNull()) && (titlePayload.getSubtitle() == null || titlePayload.getSubtitle().isJsonNull())) {
-                    return Response.failed(400, "Title and Subtitle cannot both be null", null, echo);
+                    return Response.failed(400, "Title and Subtitle cannot both be null");
                 }
                 handleApiService.handleSendTitleMessage(titlePayload.getTitle(), titlePayload.getSubtitle(), titlePayload.getFadeIn(), titlePayload.getStay(), titlePayload.getFadeOut());
-                return Response.success(null, echo);
+                return Response.success();
             }
             case "send_actionbar": {
                 MessagePayload actionMessagePayload = gson.fromJson(data, MessagePayload.class);
                 handleApiService.handleSendActionBarMessage(actionMessagePayload.getMessage());
-                return Response.success(null, echo);
+                return Response.success();
             }
             case "send_private_msg": {
                 PrivateMessagePayload privateMessagePayload = gson.fromJson(data, PrivateMessagePayload.class);
                 if ((privateMessagePayload.getNickname() == null || privateMessagePayload.getNickname().isEmpty()) && privateMessagePayload.getUuid() == null) {
-                    return Response.failed(400, PrivateMessageResponse.playerIsNull().getMessage(), PrivateMessageResponse.playerIsNull(), echo);
+                    return Response.failed(400, PrivateMessageResponse.playerIsNull().getMessage(), PrivateMessageResponse.playerIsNull());
                 }
                 PrivateMessageResponse privateMessageResponse = handleApiService.handleSendPrivateMessage(privateMessagePayload.getNickname(), privateMessagePayload.getUuid(), privateMessagePayload.getMessage());
-                return Response.success(privateMessageResponse, echo);
+                return Response.success(privateMessageResponse);
             }
             case "send_command":
-                return Response.failed(500, api + " is not supported now", null, echo);
+                return Response.failed(500, api + " is not supported now");
             case "send_rcon_command":
                 CommandPayload commandPayload = gson.fromJson(data, CommandPayload.class);
                 String result;
                 try {
                     result = GlobalContext.sendRconCommand(commandPayload.getCommand());
                     logger.info("发送 Rcon 命令: {}", commandPayload.getCommand());
-                    return Response.success(result, echo);
+                    return Response.success(result);
                 } catch (Exception e) {
                     String errorMessage = e.getMessage() != null ? e.getMessage() : "failed";
                     logger.warn("Rcon 执行命令时出现问题，命令发送失败！{}", errorMessage);
                     HashMap<Object, Object> resultData = new HashMap<>();
                     resultData.put("command", commandPayload.getCommand());
                     resultData.put("error", errorMessage);
-                    return Response.failed(400, errorMessage, resultData, echo);
+                    return Response.failed(400, errorMessage, resultData);
                 }
             default:
                 this.logger.warn(BaseConstant.UNKNOWN_API + "{}", api);
-                return Response.failed(404, BaseConstant.UNKNOWN_API + api, null, echo);
+                return Response.failed(404, BaseConstant.UNKNOWN_API + api);
         }
     }
 }
