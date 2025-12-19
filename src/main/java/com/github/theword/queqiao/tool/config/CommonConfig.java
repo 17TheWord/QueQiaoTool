3f39edc67ad27bb90758d4bc754989842e560731
17TheWord <17theword@gmail.com>
2025-12-19 16:07:02 +0800

    Merge pull request #66 from 17TheWord/fix/resource_path
    fix/resource_path

--- src/main/java/com/github/theword/queqiao/tool/config/CommonConfig.java
+++ src/main/java/com/github/theword/queqiao/tool/config/CommonConfig.java
@@ -1,6 +1,7 @@
 package com.github.theword.queqiao.tool.config;
 
 import com.github.theword.queqiao.tool.constant.BaseConstant;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
@@ -8,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.yaml.snakeyaml.Yaml;
@@ -80,7 +82,7 @@ public abstract class CommonConfig {
         } else {
             logger.warn("配置文件 {} 不存在，将生成默认配置文件。", fileName);
             try {
-                InputStream inputStream = Config.class.getClassLoader().getResourceAsStream(fileName);
+                InputStream inputStream = Config.class.getClassLoader().getResourceAsStream(BaseConstant.MOD_ID + "/" + fileName);
                 assert inputStream != null;
                 FileUtils.copyInputStreamToFile(inputStream, path.toFile());
                 logger.info("已生成默认配置文件。");
